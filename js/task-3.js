62512e5048cf5db0468d5b87cbf750812ed8acd6
Viacheslav Danylko <gamerdvv@gmail.com>
2024-09-18 10:58:19 +0300

    delete files

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-input.addEventListener('input', ({ currentTarget }) => {
-  output.textContent = currentTarget.value.trim() || 'Anonymous';
-});
